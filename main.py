9d74a118b519bb8c23c54f6cbb181b4b33fed98f
Ramgacha <chagaram02@gmail.com>
2026-08-17 09:58:05 +0900

    1

--- main.py
+++ main.py
@@ -66,9 +66,10 @@ class QuizGame:
 
         try:
             with open(self.state_file, "r", encoding="utf-8") as f:
+                self.best_score = data.get("best_score", 0)
                 data = json.load(f)
                 self.quizzes = [Quiz.from_dict(q) for q in data.get("quizzes", [])]
-                self.best_score = data.get("best_score", 0)
+
                 if not self.quizzes:
                     self.quizzes = self.get_default_quizzes()
                 print(f" 저장된 데이터를 불러왔습니다. (퀴즈 {len(self.quizzes)}개, 최고점수 {self.best_score}점)")
